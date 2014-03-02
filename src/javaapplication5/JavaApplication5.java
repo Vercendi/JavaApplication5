6a00fa32fa1d27b8fe1c35d9ae301737a683246b
Vercendi <vercendi@gmail.com>
2014-03-02 15:14:09 +0100

    Changed the message shown to end user.

--- src/javaapplication5/JavaApplication5.java
+++ src/javaapplication5/JavaApplication5.java
@@ -14,6 +14,6 @@ public class JavaApplication5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello kind user!");
+        System.out.println("Hope you'll have a great day!");
     }
 }
